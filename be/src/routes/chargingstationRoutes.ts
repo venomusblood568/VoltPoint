4058fc83973f8652947f78a759b7773c97e41d4d
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-01 10:28:58 +0530

    deletestation fix

--- be/src/routes/chargingstationRoutes.ts
+++ be/src/routes/chargingstationRoutes.ts
@@ -2,12 +2,12 @@ import  express from "express";
 import { createStation } from "../controllers/createStation";
 import { middleware } from "../middlewares/middleware";
 import { getStations } from "../controllers/getstation";
-import { deleteStaion } from "../controllers/deleteStation";
+import { deleteStation } from "../controllers/deleteStation";
 
 const router = express.Router()
 router.post("/createstation", middleware, createStation);
 router.get("/getstation",getStations);
 
-router.post("/deletestation", middleware, deleteStaion);
+router.post("/deletestation", middleware, deleteStation);
 
 export default router;
