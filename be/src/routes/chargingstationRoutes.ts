5416002cfa743e423cbd1907226a15a4b12fd19b
GOURAV_ANAND_JHA <90522738+venomusblood568@users.noreply.github.com>
2025-06-03 04:44:42 +0530

    be getstation fix

--- be/src/routes/chargingstationRoutes.ts
+++ be/src/routes/chargingstationRoutes.ts
@@ -1,13 +1,13 @@
-import  express from "express";
+import express from "express";
 import { createStation } from "../controllers/createStation";
 import { middleware } from "../middlewares/middleware";
 import { getStations } from "../controllers/getstation";
 import { deleteStation } from "../controllers/deleteStation";
 import { updateStation } from "../controllers/updateStation";
 
-const router = express.Router()
+const router = express.Router();
 router.post("/createstation", middleware, createStation);
-router.get("/getstation",getStations);
+router.get("/getstation", getStations);
 router.put("/updatestation/:stationName", middleware, updateStation);
 router.post("/deletestation", middleware, deleteStation);
 
